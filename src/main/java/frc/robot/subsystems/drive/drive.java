6128dbc94c5b309ee66ed8d9841c04fb9bb34c6f
boring89 <60728633+boring89@users.noreply.github.com>
2025-09-14 00:58:39 +0800

    Initial commit

---
+++ src/main/java/frc/robot/subsystems/drive/drive.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.drive;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class drive extends SubsystemBase{
+    
+    private final driveIO io;
+
+    public drive(driveIO io) {
+        this.io = io;
+        io.zeroHeading();
+    }
+
+    pu
+}
---
+++ src/main/java/frc/robot/subsystems/drive/driveIO.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems.drive;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+public interface driveIO {
+    //gyro
+    void zeroHeading();
+    Rotation2d getRotation2d();
+    double getHeading();
+    double getTurnRate();
+    //mooduleState
+    SwerveModuleState[] getModuleStates();
+    SwerveModulePosition[] getModulePositions();
+    void setModuleStates(SwerveModuleState[] state);
+    //encoders
+    void resetEncoders();
+    void stopModules();
+
+}
